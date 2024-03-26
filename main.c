ccdaf884edd1a0ed3bc97759fd89ef398e523caa
Zhao Lixiuqi <e0774464@u.nus.edu>
2024-03-26 16:51:16 +0800

    Update main.c
    Add audio function to the robot

--- main.c
+++ main.c
@@ -40,10 +40,21 @@
  * Application main thread
  *---------------------------------------------------------------------------*/
 
-volatile int moving_status = 1;
-volatile int task_status = 0;
+volatile int moving_status = 0;
 volatile double gear_multiplier[4] = {0.2, 0.5, 0.8, 1.2};
+volatile double rl_turn[4] = {0.5, 0.7, 0.9, 1.2};
 volatile int gear_level = 2;
+
+// This part is for the audio information
+uint8_t megMelody[] = {1, 3, 5, 6, 5, 0, 1, 2, 1, 3, 0, 3, 5, 6, 7, 6, 5, 3, 2, 1, 2, 3, 1};
+uint8_t megDurations[] = {1, 1, 1, 3, 1, 3, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1};
+uint8_t megNumNotes = sizeof(megMelody) / sizeof(megMelody[0]);
+	
+uint8_t megMelody_1[] = {5, 6, 7, 6, 7, 5, 2, 5, 0};
+uint8_t megDurations_1[] = {1, 1, 1, 2, 1, 2, 1, 3, 5};
+uint8_t megNumNotes_1 = sizeof(megMelody_1) / sizeof(megMelody_1[0]);
+
+uint8_t end_point = 0;
 
 void InitGPIO(void)
 {
@@ -194,10 +205,10 @@ void UART2_IRQHandler(void) {
             moving_status = 1;
             break;
           case 0x12: //a, move left
-            TPM0_C5V = 0.5 * TPM0_MOD * gear_multiplier[gear_level];  //50% duty 
+            TPM0_C5V = 0.5 * TPM0_MOD * rl_turn[gear_level];  //50% duty 
             TPM0_C2V = 0;
             TPM0_C3V = 0;
-            TPM0_C4V = 0.5 * TPM0_MOD * gear_multiplier[gear_level];
+            TPM0_C4V = 0.5 * TPM0_MOD * rl_turn[gear_level];
             moving_status = 1;
             break;
           case 0x13: //s, move backward
@@ -209,8 +220,8 @@ void UART2_IRQHandler(void) {
             break;
           case 0x14: //d, move right
             TPM0_C5V = 0;
-            TPM0_C2V = 0.5 * TPM0_MOD * gear_multiplier[gear_level];
-            TPM0_C3V = 0.5 * TPM0_MOD * gear_multiplier[gear_level];  //50% duty
+            TPM0_C2V = 0.5 * TPM0_MOD * rl_turn[gear_level];
+            TPM0_C3V = 0.5 * TPM0_MOD * rl_turn[gear_level];  //50% duty
             TPM0_C4V = 0;
             moving_status = 1;
             break;
@@ -224,17 +235,26 @@ void UART2_IRQHandler(void) {
           case 0x16: //gear up
             if (gear_level < 3)
               gear_level++;
+						moving_status = 0;
             break;
           case 0x17: //gear down
             if (gear_level >= 1)
               gear_level--;
+						moving_status = 0;
             break;
+					case 0x21:
+						end_point = 1; //reached end point
+					  moving_status = 0;
+					  break;
+					case 0x22:
+						end_point = 0; //replay the running music
+					  moving_status = 0;
+					  break;
           default:
             TPM0_C5V = 0;
             TPM0_C2V = 0;
             TPM0_C3V = 0;
             TPM0_C4V = 0;
-            moving_status = 0;
         }
     }
 }
@@ -315,32 +335,46 @@ void back_led (void *argument) {
   for (;;) {
 		if (moving_status == 1){
 			PTC->PSOR = MASK(BACK_LED);
-			osDelay(250);
+			osDelay(500);
 			PTC->PCOR = MASK(BACK_LED);
-			osDelay(250);
+			osDelay(500);
 		}
 		if (moving_status == 0){
 			PTC->PSOR = MASK(BACK_LED);
-			osDelay(125);
+			osDelay(250);
 			PTC->PCOR = MASK(BACK_LED);
-			osDelay(125);
+			osDelay(250);
 		}
 	}
 }
 
-int musicalnotes(int note_index) {
-    // Array of note frequencies corresponding to musical notes C4 to B4
-    int notes[7] = {262, 294, 330, 349, 392, 440, 494};
-    if (note_index >= 0 && note_index < 7) {
-        return notes[note_index];
-    } else {
-        return 0; // Return 0 for rests or invalid indices
-    }
+int musicalnotes(int i)
+{
+	if (i == 0)
+		return 0;
+	else if (i == 1)
+		return 262;
+	else if (i == 2)
+		return 294;
+	else if (i == 3)
+		return 330;
+	else if (i == 4)
+		return 349;
+	else if (i == 5)
+		return 392;
+	else if (i == 6)
+		return 440;
+	else if (i == 7)
+		return 494;
+	else 
+		return 0;
 }
 
 int freq_2_MOD(int freq)
 {
-    return CLOCK_FREQ/PRE_SCALAR/freq;
+	if (freq == 0)
+		return 0;
+	return CLOCK_FREQ/PRE_SCALAR/freq;
 }
 
 static void Delay(void) {
@@ -348,6 +382,37 @@ static void Delay(void) {
     for (a=0; a < 4000000; a++) {
         __NOP(); 
     }
+}
+
+void buzzer (void *argument) {
+	int i = 0;
+	int j = 0;
+	
+	for(;;)
+	{
+		if (end_point == 0) {
+			if (i == megNumNotes)
+				i = 0;
+			TPM1->MOD = freq_2_MOD(musicalnotes(megMelody[i]));
+			TPM1_C0V = freq_2_MOD(musicalnotes(megMelody[i])) / 2;
+			osDelay(megDurations[i]*300);
+			i++;
+		} 
+		else if (end_point == 1)
+		{
+			if (j == megNumNotes_1) {
+				j = 0;
+			    end_point ++;
+			}
+			TPM1->MOD = freq_2_MOD(musicalnotes(megMelody_1[j]));
+			TPM1_C0V = freq_2_MOD(musicalnotes(megMelody_1[j])) / 2;
+			osDelay(megDurations_1[j]*300);
+			j++;
+		}
+		else {
+			TPM1_C0V = 0;
+		}
+	} 
 }
 
  int main (void) {
@@ -365,6 +430,7 @@ static void Delay(void) {
   
     osKernelInitialize();                 // Initialize CMSIS-RTOS
     osThreadNew(front_led, NULL, NULL);    // Create application main thread
+	  osThreadNew(buzzer, NULL, NULL);      // Create application main thread
     osThreadNew(back_led, NULL, NULL);    // Create application main thread
     osKernelStart();                      // Start thread execution
     for (;;) {}
